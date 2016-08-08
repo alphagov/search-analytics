93ecb127e8ad8e2ec55ed1cb46ebbed9089ecd82
Mat Moore <mat.moore@digital.cabinet-office.gov.uk>
2016-08-08 12:32:55 +0000

    Rename environment variable to reflect new behaviour

--- nightly-run.sh
+++ nightly-run.sh
@@ -5,8 +5,8 @@ rm -f page-traffic.dump
 PYTHONPATH=. python scripts/fetch.py page-traffic.dump 14
 SEARCH_NODE=$(/usr/local/bin/govuk_node_list -c search --single-node)
 ssh deploy@${SEARCH_NODE} '(cd /var/apps/rummager; govuk_setenv rummager bundle exec ./bin/bulk_load page-traffic)' < page-traffic.dump
-ssh deploy@${SEARCH_NODE} '(cd /var/apps/rummager; SKIP_LINKS_INDEXING_TO_PREVENT_TIMEOUTS=1 RUMMAGER_INDEX=mainstream govuk_setenv rummager bundle exec rake rummager:migrate_index)'
-ssh deploy@${SEARCH_NODE} '(cd /var/apps/rummager; SKIP_LINKS_INDEXING_TO_PREVENT_TIMEOUTS=1 RUMMAGER_INDEX=detailed govuk_setenv rummager bundle exec rake rummager:migrate_index)'
-ssh deploy@${SEARCH_NODE} '(cd /var/apps/rummager; SKIP_LINKS_INDEXING_TO_PREVENT_TIMEOUTS=1 RUMMAGER_INDEX=government govuk_setenv rummager bundle exec rake rummager:migrate_index)'
-ssh deploy@${SEARCH_NODE} '(cd /var/apps/rummager; SKIP_LINKS_INDEXING_TO_PREVENT_TIMEOUTS=1 RUMMAGER_INDEX=service-manual govuk_setenv rummager bundle exec rake rummager:migrate_index)'
-ssh deploy@${SEARCH_NODE} '(cd /var/apps/rummager; SKIP_LINKS_INDEXING_TO_PREVENT_TIMEOUTS=1 RUMMAGER_INDEX=all govuk_setenv rummager bundle exec rake rummager:clean)'
+ssh deploy@${SEARCH_NODE} '(cd /var/apps/rummager; LOG_FAILED_LINKS_LOOKUP_AND_CONTINUE=1 RUMMAGER_INDEX=mainstream govuk_setenv rummager bundle exec rake rummager:migrate_index)'
+ssh deploy@${SEARCH_NODE} '(cd /var/apps/rummager; LOG_FAILED_LINKS_LOOKUP_AND_CONTINUE=1 RUMMAGER_INDEX=detailed govuk_setenv rummager bundle exec rake rummager:migrate_index)'
+ssh deploy@${SEARCH_NODE} '(cd /var/apps/rummager; LOG_FAILED_LINKS_LOOKUP_AND_CONTINUE=1 RUMMAGER_INDEX=government govuk_setenv rummager bundle exec rake rummager:migrate_index)'
+ssh deploy@${SEARCH_NODE} '(cd /var/apps/rummager; LOG_FAILED_LINKS_LOOKUP_AND_CONTINUE=1 RUMMAGER_INDEX=service-manual govuk_setenv rummager bundle exec rake rummager:migrate_index)'
+ssh deploy@${SEARCH_NODE} '(cd /var/apps/rummager; LOG_FAILED_LINKS_LOOKUP_AND_CONTINUE=1 RUMMAGER_INDEX=all govuk_setenv rummager bundle exec rake rummager:clean)'
