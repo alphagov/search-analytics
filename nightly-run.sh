257dda830c5bf5a91fe9a166ada79ed3176a6b5c
David Henry <david@decoybecoy.com>
2017-10-02 15:47:27 +0100

    Update page traffic load command

--- nightly-run.sh
+++ nightly-run.sh
@@ -8,7 +8,7 @@ pip install -r requirements.txt
 rm -f page-traffic.dump
 PYTHONPATH=. python scripts/fetch.py page-traffic.dump 14
 SEARCH_NODE=$(/usr/local/bin/govuk_node_list -c search --single-node)
-ssh deploy@${SEARCH_NODE} '(cd /var/apps/rummager; govuk_setenv rummager bundle exec ./bin/bulk_load page-traffic)' < page-traffic.dump
+ssh deploy@${SEARCH_NODE} '(cd /var/apps/rummager; govuk_setenv rummager bundle exec ./bin/page_traffic_load)' < page-traffic.dump
 
 ssh deploy@${SEARCH_NODE} '(cd /var/apps/rummager; PROCESS_ALL_DATA=true RUMMAGER_INDEX=mainstream govuk_setenv rummager bundle exec rake rummager:update_popularity)'
 ssh deploy@${SEARCH_NODE} '(cd /var/apps/rummager; PROCESS_ALL_DATA=true RUMMAGER_INDEX=detailed govuk_setenv rummager bundle exec rake rummager:update_popularity)'
