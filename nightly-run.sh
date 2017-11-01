86122c19b8913bf210807718bc0974563fe0a18a
Suzanne Hamilton <suzanne.hamilton@digital.cabinet-office.gov.uk>
2017-11-01 15:56:02 +0000

    Temporarily disable popularity update job
    Prevent the popularity update from running overnight while we
    investigate the root cause of an incident which may have been caused by
    this job.

--- nightly-run.sh
+++ nightly-run.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-set -e
-set -o pipefail
-
-if [ \! -d ENV ]; then virtualenv ENV; fi
-. ENV/bin/activate
-pip install -r requirements.txt
-rm -f page-traffic.dump
-PYTHONPATH=. python scripts/fetch.py page-traffic.dump 14
-SEARCH_NODE=$(/usr/local/bin/govuk_node_list -c search --single-node)
-ssh deploy@${SEARCH_NODE} '(cd /var/apps/rummager; govuk_setenv rummager bundle exec ./bin/page_traffic_load)' < page-traffic.dump
-
-ssh deploy@${SEARCH_NODE} '(cd /var/apps/rummager; PROCESS_ALL_DATA=true RUMMAGER_INDEX=mainstream govuk_setenv rummager bundle exec rake rummager:update_popularity)'
-ssh deploy@${SEARCH_NODE} '(cd /var/apps/rummager; PROCESS_ALL_DATA=true RUMMAGER_INDEX=detailed govuk_setenv rummager bundle exec rake rummager:update_popularity)'
-ssh deploy@${SEARCH_NODE} '(cd /var/apps/rummager; PROCESS_ALL_DATA=true RUMMAGER_INDEX=government govuk_setenv rummager bundle exec rake rummager:update_popularity)'
-ssh deploy@${SEARCH_NODE} '(cd /var/apps/rummager; RUMMAGER_INDEX=govuk govuk_setenv rummager bundle exec rake rummager:update_popularity)'
-
-ssh deploy@${SEARCH_NODE} '(cd /var/apps/rummager; govuk_setenv rummager bundle exec rake rummager:sync_govuk)'
+# set -e
+# set -o pipefail
+#
+# if [ \! -d ENV ]; then virtualenv ENV; fi
+# . ENV/bin/activate
+# pip install -r requirements.txt
+# rm -f page-traffic.dump
+# PYTHONPATH=. python scripts/fetch.py page-traffic.dump 14
+# SEARCH_NODE=$(/usr/local/bin/govuk_node_list -c search --single-node)
+# ssh deploy@${SEARCH_NODE} '(cd /var/apps/rummager; govuk_setenv rummager bundle exec ./bin/page_traffic_load)' < page-traffic.dump
+#
+# ssh deploy@${SEARCH_NODE} '(cd /var/apps/rummager; PROCESS_ALL_DATA=true RUMMAGER_INDEX=mainstream govuk_setenv rummager bundle exec rake rummager:update_popularity)'
+# ssh deploy@${SEARCH_NODE} '(cd /var/apps/rummager; PROCESS_ALL_DATA=true RUMMAGER_INDEX=detailed govuk_setenv rummager bundle exec rake rummager:update_popularity)'
+# ssh deploy@${SEARCH_NODE} '(cd /var/apps/rummager; PROCESS_ALL_DATA=true RUMMAGER_INDEX=government govuk_setenv rummager bundle exec rake rummager:update_popularity)'
+# ssh deploy@${SEARCH_NODE} '(cd /var/apps/rummager; RUMMAGER_INDEX=govuk govuk_setenv rummager bundle exec rake rummager:update_popularity)'
+#
+# ssh deploy@${SEARCH_NODE} '(cd /var/apps/rummager; govuk_setenv rummager bundle exec rake rummager:sync_govuk)'
