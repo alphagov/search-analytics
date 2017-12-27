a30d4f8e72b9db5c3866c07949db0d5d1020561d
Paul Bowsher <paul.bowsher@gmail.com>
2017-12-27 07:29:27 +0000

    Fix a bash syntax error causing the job to fail

--- nightly-run.sh
+++ nightly-run.sh
@@ -10,7 +10,7 @@ if [[ -z $SKIP_TRAFFIC_LOAD ]]; then
   rm -f page-traffic.dump
   PYTHONPATH=. python scripts/fetch.py page-traffic.dump 14
   ssh deploy@${SEARCH_NODE} '(cd /var/apps/rummager; govuk_setenv rummager bundle exec ./bin/page_traffic_load)' < page-traffic.dump
-  ssh deploy@${SEARCH_NODE} '(cd /var/apps/rummager; govuk_setenv rummager bundle exec rake rummager:clean RUMMAGER_INDEX=page-traffic'
+  ssh deploy@${SEARCH_NODE} '(cd /var/apps/rummager; govuk_setenv rummager bundle exec rake rummager:clean RUMMAGER_INDEX=page-traffic)'
 fi
 
 ssh deploy@${SEARCH_NODE} '(cd /var/apps/rummager; PROCESS_ALL_DATA=true RUMMAGER_INDEX=mainstream govuk_setenv rummager bundle exec rake rummager:update_popularity)'
