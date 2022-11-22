cebf49bead605a97f11f486d8a56d407b9d438af
Graham Lewis <44037625+gclssvglx@users.noreply.github.com>
2022-11-22 13:56:58 +0000

    Fix pylint issues
    Fixes the following pylint errors...
    
    analytics_fetcher/ga.py:30:0: R0205: Class 'GAData' inherits from object, can be safely removed from bases in python3 (useless-object-inheritance)
    analytics_fetcher/ga.py:36:24: C0209: Formatting a regular string which could be a f-string (consider-using-f-string)
    analytics_fetcher/ga.py:30:0: R0903: Too few public methods (1/2) (too-few-public-methods)

--- analytics_fetcher/ga.py
+++ analytics_fetcher/ga.py
@@ -27,17 +27,13 @@
 """
 
 
-class GAData(object):
+class GAData():
     """Gets page view data via the Google API"""
     def __init__(self, ga_client, date):
         self.client = ga_client
         self.date = date
         self.date_idstr = date.strftime("%Y%m%d")
-        self.date_str = '%04d-%02d-%02dT00:00:00Z' % (
-            self.date.year,
-            self.date.month,
-            self.date.day,
-        )
+        self.date_str = f'{self.date.year}-{self.date.month:02d}-{self.date.day:02d}T00:00:00Z'
 
     def fetch_traffic_info(self):
         """Fetch info on views of pages.
@@ -50,17 +46,7 @@ class GAData(object):
         """
         not_found_title = 'Page not found - 404 - GOV.UK'
         result = {}
-        for row in self.client.fetch(
-            'search', self.date,
-            metrics='ga:uniquePageViews',
-            dimensions='ga:pagePath,ga:pageTitle',
-            sort='-ga:uniquePageViews',
-            name_map={
-                'uniquePageViews': 'views',
-                'pagePath': 'path',
-                'pageTitle': 'title',
-            },
-        ):
+        for row in self.get_traffic_from_api():
             path = row['path']
             views = row['views']
             title = row['title']
@@ -72,3 +58,19 @@ class GAData(object):
                 item[0] += views
                 item[1] = (item[1] and not_found)
         return result
+
+    def get_traffic_from_api(self):
+        """Searches the Google API for page view data and returns
+            all matching rows.
+        """
+        return self.client.fetch(
+            'search', self.date,
+            metrics='ga:uniquePageViews',
+            dimensions='ga:pagePath,ga:pageTitle',
+            sort='-ga:uniquePageViews',
+            name_map={
+                'uniquePageViews': 'views',
+                'pagePath': 'path',
+                'pageTitle': 'title',
+            },
+        )
